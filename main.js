c562eb6016a883eb1d09215e02c9d553aedb6243
Vitor Lopes Nunes <79660123+vitorlopesnunes21@users.noreply.github.com>
2024-05-23 10:57:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom(seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('elemento não encontrado ou seletor inválido')
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    };
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.key)
+        console.log(evento.code)
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa')
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+}
